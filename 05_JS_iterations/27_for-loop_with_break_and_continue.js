40d349eb7f76d75a69cb93ab6e5752003259d6f1
IshanYadav1008 <ishanyadav1008@gmail.com>
2024-07-19 06:49:08 +0000

    break and continue

--- 05_JS_iterations/27_for-loop_with_break_and_continue.js
+++ 05_JS_iterations/27_for-loop_with_break_and_continue.js
@@ -188,3 +188,123 @@ for (let index = 0; index < myArray.length; index++) {
     
 
 */
+
+console.log("---------------------------------------");
+
+// --------------------------------------------------------------------------------------------------------
+
+/* Ab hum dekhte hai kuch keywords ke baare mai:
+
+   Ab maanlo ki humne 1 to 10 values print kari yaa poora array print karwa diya toh aisa jarort nhi hai ki
+   hume loop ko poora hi print karwana hai. 
+
+   Humne maanlo ek baar loop ko start kar diya toh usko end tak hi kthm karna hai, kayi baar kya hota hai ki
+   maanlo beech mai koi condition aa gyi toh us condition ke basis pr hume loop ko remove karna hai.
+
+   Toh iska liye 2-Important keywords hote hai: "break" and "continue"
+
+*/
+
+// ********************************** "break" and "continue" ****************************************
+
+for (let i = 1; i <= 20; i++) {
+
+    console.log(`Value of i is ${i}`);  
+}
+/* Output ==> Value of i is 1
+              Value of i is 2
+              Value of i is 3
+              Value of i is 4
+              Value of i is 5
+              Value of i is 6
+              Value of i is 7
+              Value of i is 8
+              Value of i is 9
+              Value of i is 10
+              Value of i is 11
+              Value of i is 12
+              Value of i is 13
+              Value of i is 14
+              Value of i is 15
+              Value of i is 16
+              Value of i is 17
+              Value of i is 18
+              Value of i is 19
+              Value of i is 20 
+
+    */
+console.log("---------------------------------------");
+
+/* ******************************************** break ***********************************************
+
+   Kisi bhi control flow ko break karna ho toh hum "break" likhte hai.
+
+   NOTE: Ab hum ye chahate hai ki jaise hi loop mai 5 aaye toh hum loop se bahar aa jaaye.
+
+*/
+
+console.log("Output after using break");
+console.log("");
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i == 5){
+        console.log("Dectected 5");
+        break;
+    }
+
+    console.log(`Value of i is ${i}`);  
+}
+/* Output ==> Output after using break
+
+              Value of i is 1
+              Value of i is 2
+              Value of i is 3
+              Value of i is 4
+              Dectected 5
+*/
+
+console.log("---------------------------------------");
+
+/* ******************************************** continue *********************************************** 
+
+   "continue" ek baar iteration ko skip kar deta hai
+*/
+
+console.log("Output after using continue");
+console.log("");
+
+for (let i = 1; i <= 20; i++) {
+
+    if (i == 5){
+        console.log("Dectected 5");
+        continue
+    }
+
+    console.log(`Value of i is ${i}`);  
+}
+
+/* Output ==> Output after using continue
+
+              Value of i is 1
+              Value of i is 2
+              Value of i is 3
+              Value of i is 4
+              Dectected 5       // Jaise yha sirf ye print hua iske baad isne aage ki iteration ko skip kr diya.
+              Value of i is 6
+              Value of i is 7
+              Value of i is 8
+              Value of i is 9
+              Value of i is 10
+              Value of i is 11
+              Value of i is 12
+              Value of i is 13
+              Value of i is 14
+              Value of i is 15
+              Value of i is 16
+              Value of i is 17
+              Value of i is 18
+              Value of i is 19
+              Value of i is 20
+
+*/
