e13b5b119660bec8e73dc55c9695cace58c294ef
IshanYadav1008 <ishanyadav1008@gmail.com>
2024-07-29 10:56:27 +0000

    Used forof and  forin loops with Map()

--- 05_JS_iterations/29_high-order-array-loops.js
+++ 05_JS_iterations/29_high-order-array-loops.js
@@ -170,24 +170,200 @@ console.log("------------------------------------------");
 
 // ------------------------------------------------------------------------------------------------
 
-// Kya hum "Object" pr "forof" loop lga sakte hai ?
+/* Kya hum "Object" pr "forof" loop lga sakte hai ?
 
 const myObject = {         // Ye hmara object ban gya or isme humne 2-values bhi le li.
     'game1': 'NFS',
     'gane2': 'Spiderman'
 }
 
-// An es "myObject" object par forof loop kaise lagega ?
+Ab es "myObject" object par "forof" loop kaise lagega ?
 
 for (const [key, value] of myObject) {
     console.log(key, ':-', value);       
 }
-/* Output ==> Error (myObject is not iterable)
+Output ==> Error (myObject is not iterable)
    
-Yha ye bol rha hai ki ye jo "myObject" hai yha par toh ye iterable nhi hai.
+Yha ye bol rha hai ki ye jo "myObject" hai yha par, toh ye iterable nhi hai.
 
 Note: Toh yaha par "Maps()" iterable hai lekin "Objects" iterable nhi hai.
 
 ***imp ==> lekin Objects ko iterate karwane ke dosre tarike hote hai, jo ki hum baad mai padenge.
 
 */
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* ***************************************** forin ********************************************** 
+
+   Ab hum "forin" loop ka use karke "Object" ki "keys" ko print karwa rhe hai.
+*/
+
+console.log("Printing keys of myObject using forin loop");
+const myObject = {
+    js   : 'jsavascript',
+    cpp  : 'c++',
+    rb   : 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject) {
+    console.log(key);  // Ab hum key ko print karke dekhenge 
+}
+/* Output ==> Printing keys of myObject using forin loop
+              js
+              cpp
+              rb
+              swift
+
+Ab hmre paas "keys" toh saari aa gyi hai.
+*/
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* Lekin hume sirf "keys" nhi balki uske saath "Object" bhi chyiae. Toh ab Object kaise nikale ?
+
+   Ab jab hmare paas keys aa gyi hai toh object nikalne ka tarika bhi bhut easy hai.
+*/
+
+console.log("Printing values of keys using forin loop");
+const myObject_1 = {
+    js   : 'javascript',
+    cpp  : 'c++',
+    rb   : 'ruby',
+    swift: 'swift by apple'
+}
+
+for (const key in myObject_1) {
+     console.log(myObject_1[key]);  /* Hume kya krna hai ? isme "keys" ki place pr "myObject" ko lena hai or bolna h ki
+                                       "myObject" ke andar jo "key" hai uski value hume print krwa do, toh hmare pass
+                                       "key" bhi aa gyi or "Object" bhi aa gya. 
+                                       
+    or
+
+    console.log(`${key} shortcut is for ${myObject_1[key]}`);
+
+    Output ==>
+
+    Printing values of keys using forin loop
+    js shortcut is for javascript
+    cpp shortcut is for c++
+    rb shortcut is for ruby
+    swift shortcut is for swift by apple
+    */
+}
+/* Output ==>
+
+    Printing values of keys using forin loop
+    javascript
+    c++
+    ruby
+    swift by apple
+*/
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* ***************************** forin loop with Array ************************************* 
+
+Kya hum "forin" loop ko "Arrays" ke liye bhi use kar sakte hai ? */
+
+
+console.log("Printing keys of array using forin loop");
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    console.log(key);
+}
+/* Output ==> Printing keys of array using forin loop
+              0
+              1
+              2
+              3
+              4 
+
+Toh yha forin mai Array ki keys print ho rhi hai. Ji haan Array ki bhi keys hoti hai or vo zero(0)
+se start hoti hai jinhe hum index numbers bolte hai.
+
+
+Ques) Difference between forof or forin ?
+
+Ans.) "forof" mai array ki direct values print ho rhi thi jabki "forin" mai array ki "key"s print ho 
+      rhi hai.
+*/
+
+console.log("------------------------------------------");
+
+console.log("Printing key's values of array using forin loop");
+const programming_1 = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming_1) {
+    console.log(programming_1[key]);
+}
+/* Output ==> Printing key's values of array using forin loop
+              js
+              rb
+              py
+              java
+              cpp
+*/
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* Kya hum "Map()" pr "forin" loop lga sakte hai kya ? */
+
+console.log("Using forin loop with map");
+
+const map_3 = new Map()                      
+map_3.set('IN',  "INDIA")                    
+map_3.set('USA', "United State of America")
+map_3.set('FR',  "France")
+map_3.set('IN',  "INDIA")
+
+for (const key in map_3) {
+    console.log(key);
+}
+
+/* Output =>  Using forin loop with map
+
+Kuch bhi output nhi aayega kyuki "Map()" "forin" loop ke sath iterable nhi hai or jab koi cheez
+iterable nhi hai toh phir use hum loop mai nhi likh sakte.
+
+*/
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* *************************************** for each loop **************************************** 
+
+Actually kuch loops "arrays" ke andar direct hi properties mai add kar diye jaate hai:
+
+* Goto Google ==> right click ==> inspect ==> console ==> write the below line of code :-
+
+* const coding = ["js", "ruby", "java", "python", "cpp"] --> Enter
+* coding                                                 --> Enter
+
+Ab jaise hi hum iska dropdown ko open karenge toh iske prototypes open ho jayenge:
+
+* [[Prototype]]: Array(0) ==> Phir hum es prototype ko open karenge toh hume kaaf kuch cheezen dekhai 
+                              dengi.
+
+                              Isme hume "map" or "forEach" bhi milega, toh ye loop ka hi ek parameter hai
+                              jo bydefault hi inject kar rakha hai kyuki sabko pta hai Arrays agar ayenge
+                              toh loop toh lagana hi padega.
+
+                              "forEach" higher order function hai or ise use karna bhut wasy hai or sbse jyda
+                              yhi use hota hai.
+
+*/
+
+
+const coding = ["js", "ruby", "java", "python", "cpp"]
