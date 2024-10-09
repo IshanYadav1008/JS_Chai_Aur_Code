406dc55b8c8792ab9e926f3c729bfa55f9700e03
IshanYadav1008 <ishanyadav1008@gmail.com>
2024-10-09 12:53:07 +0000

    learned about filter() operation

--- 05_JS_iterations/30_filter_map_and_reduce.js
+++ 05_JS_iterations/30_filter_map_and_reduce.js
@@ -141,3 +141,63 @@ IMP*** ==> Ye same cheez "forEach()" mai bhi hoti hai lekin forEach values
 const newNums = myNums.filter( (num) => num > 4 ) // "newNUmns" mai humne ise store krwa liya hai.
  
 console.log(newNums); // Ab newNums mai value kya aayegi ? 
+
+/* Output ==> [ 5, 6, 7, 8, 9, 10 ]  // Toh "newNums" mai ye value aa gyi hai.
+
+   Toh ye hume samaj mai mai aa gya hai ki jab bhi hum "filter()" use karte hai toh filter ke andar
+   hume ek callback function ( (num) => num > 4 ) milega jiske andar har ek value ko each access --> (num)
+   kiya jayega lekin uske baad hume deni padti hai ek condition (num > 4), toh jo-jo log us condition ko
+   satisfy karenge or true hogi condition toh vo-vo values toh return ki jayengi varna vo values nhi
+   return ki jayengi or isi ko filter ka basics bola jata hai. Or isko bhut jagah hum use karenge or bhut
+   jyda impletation hoga iska.
+
+*/
+
+console.log("------------------------------------------");
+
+// ------------------------------------------------------------------------------------------------
+
+/* Ab isme thodi si problems bhi aati hai logon ko. Ab vo kaise ?
+   
+   Hume filter() ke saath arrow functions ko dosri tarah se use karke dekhte hai jo ki hum pehle
+   bhi krke dekh chuke hai, lekin ab filter k saath krke dekhenge.
+*/
+console.log("Empty array is coming because we are not using return keyword inside the scope to return the value");
+
+const myNums_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNumns_1 = myNums_1.filter( (num) => {
+    num > 4
+} )
+console.log(newNumns_1);
+
+/* Output ==> [] --> Ab ye array empty kyu aaya? Ye cheez hume humne hmare "arrow" waale topic mai
+                     discuss kari thi.
+
+                     Ki jab hum paranthesis () laga rhe the tb toh value khud se return ho rhi thi
+                     lekin jab curly-braces {} lga rhe hai toh khud se value retun nhi ho rhi hai
+                     kyuki humne cruly-braces lga kr ek scope start kar diya hai or agr hum scope
+                     start kar denge toh phit hum "return" keyword likhna hi padega.
+*/
+
+console.log("------------------------------------------");
+
+console.log("We are usign return keyword explicitely inside the scope to return the value of num");
+
+const myNums_2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNumns_2 = myNums_2.filter( (num) => {
+    return num > 4   // Curly-braces {} lgane ka mtlb hai scope or scope ke andar ki value ko
+} )                  // hume explicitely "return" keyword likh kar return karna padega.
+console.log(newNumns_2);
+
+/* Output ==> We are usign return keyword explicitely inside the scope to return the value of num
+              [ 5, 6, 7, 8, 9, 10 ] 
+*/
+
+/* *** Very Imp Note *** ==> 
+    
+1) Agr hum ye --> {} (scope) open karte hai toh hume "return" keyword use karna padega.
+
+2) or agr hum ye --> {} (scope) open nhi karte hai toh toh hum directly ye (num > 4) line likh
+   sakte hai yaa jo bhi hmari condition hai toh ye return ho jayega. Lekin ye one line condition
+   ke liye hai, agr usse jyda line hongi toh phir hume scope lagana hi padega. 
+*/
